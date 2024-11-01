ad1e5cc5eaf3493c49fff75a2a1226f6de0b7788
Hernan Miranda <mirandahernan2002@gmail.com>
2024-11-01 16:22:34 -0300

    tecnologias terminado

--- src/components/Tehcnology.jsx
+++ src/components/Tehcnology.jsx
@@ -1,13 +1,62 @@
 import React from "react";
+import { FaAndroid, FaJava } from "react-icons/fa";
+import { SiKotlin, SiJetpackcompose } from "react-icons/si";
+
 function Technology(){
     return(
-        <div className="flex-col flex text-white items-start gap-2">
-            <h2 className="items-start font-bold text-2xl m-0">Tecnologias</h2>
+        <div className="flex-col flex text-white items-start gap-2 font-bold">
+            <h2 className="items-start text-2xl m-0">Tecnologias</h2>
 
-            <div className="flex-wrap flex items-start gap-2">
+            <div className="flex-wrap flex gap-3">
+                <div className="flex-wrap flex items-start bg-green-700 rounded-md p-3 gap-2">
 
+                    <span>
+                        <div className="text-2xl">
+                            <FaAndroid/>
+                        </div>
+                    </span>
+
+                    <p>Android Studio</p>
+
+                </div>
+
+                <div className="flex-wrap flex items-start bg-green-700 rounded-md p-3 gap-2">
+
+                    <span>
+                        <div className="text-2xl">
+                            <SiKotlin/>
+                        </div>
+                    </span>
+
+                    <p>Kotlin</p>
+
+                </div>
+
+                <div className="flex-wrap flex items-start bg-green-700 rounded-md p-3 gap-2">
+
+                    <span>
+                        <div className="text-2xl">
+                            <FaJava/>
+                        </div>
+                    </span>
+
+                    <p>Java</p>
+
+                </div>
+
+                <div className="flex-wrap flex items-start bg-green-700 rounded-md p-3 gap-2">
+
+                    <span>
+                        <div className="text-2xl">
+                            <SiJetpackcompose/>
+                        </div>
+                    </span>
+
+                    <p>Jetpack Compose</p>
+
+                </div>
             </div>
-        </div>
+        </div>    
     )
 }
 
