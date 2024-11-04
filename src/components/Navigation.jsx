a9262f805b59ef6f30a814bee90aca6a446a165a
Hernan Miranda <mirandahernan2002@gmail.com>
2024-11-04 16:39:37 -0300

    cambios en proyectos

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 function Navigation() {
   return (
-    <nav className="bg-green-600 p-2">
+    <nav className="bg-[#0F0F0F] p-2">
       <ul className="flex justify-center md:justify-start gap-4">
         <li>
-          <a href="#home" className="text-gray-800 hover:text-white">
+          <a href="#home" className="text-gray-400 hover:text-white">
             Inicio
           </a>
         </li>
         <li>
-          <a href="#services" className="text-gray-800 hover:text-white">
+          <a href="#services" className="text-gray-400 hover:text-white">
             Proyectos
           </a>
         </li>
         <li>
-          <a href="#contact" className="text-gray-800 hover:text-white">
+          <a href="#contact" className="text-gray-400 hover:text-white">
             Experiencia
           </a>
         </li>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Proyecto1 from '../proyecto1.png'
+import Proyecto2 from '../proyecto2.png'
 import {
   FaFileSignature,
   FaAndroid,
@@ -8,6 +9,7 @@ import {
   FaGithub,
 } from 'react-icons/fa'
 import { SiKotlin, SiJetpackcompose, SiFirebase } from 'react-icons/si'
+import { AiFillApi } from 'react-icons/ai'
 
 function Projects() {
   return (
@@ -63,6 +65,57 @@ function Projects() {
             </div>
             <img
               src={Proyecto1}
+              alt=""
+              className="h-[300px] w-auto rounded-2xl max-w-full object-cover"
+            />
+          </div>
+        </div>
+        <div className="min-w-[30em] flex-row flex w-full justify-start gap-4">
+          <div className="flex w-full flex-row justify-start gap-4">
+            <div className="flex-row flex w-full justify-start gap-4 items-start">
+              <span className="bg-green-800 w-12 h-12 rounded-full flex items-center justify-center text-2xl aspect-square"></span>
+              <div className="flex-col flex justify-start items-start w-full gap-2">
+                <strong className="text-2x1">Rick And Morty App</strong>
+                <p>Proyecto personal</p>
+                <p className="m-0 leading-5 tracking-normal">
+                  Muestra a todos los personajes y detalles de Rick y Morty
+                </p>
+                <div className="flex-wrap flex justify-start gap-2">
+                  <span className="inline-flex items-center box-border flex-shrink-0 whitespace-nowrap selection-none gap-1 p-[4px] rounded-xl bg-gray-700">
+                    <div className="box-border">
+                      <SiKotlin />
+                    </div>
+                    Kotlin
+                  </span>
+                  <span className="inline-flex items-center box-border flex-shrink-0 whitespace-nowrap selection-none gap-1 p-[4px] rounded-xl bg-gray-700">
+                    <div className="box-border">
+                      <AiFillApi />
+                    </div>
+                    RickAndMortyAPI
+                  </span>
+                  <span className="inline-flex items-center box-border flex-shrink-0 whitespace-nowrap selection-none gap-1 p-[4px] rounded-xl bg-gray-700">
+                    <div className="box-border">
+                      <FaSyringe />
+                    </div>
+                    Hilt
+                  </span>
+                </div>
+                <div className="flex flex-row items-start justify-start gap-2">
+                  <a
+                    href="https://github.com/hernanMiranda22/RickAndMortyApp"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="border-green-950 rounded-3xl border-2"
+                  >
+                    <button className="cursor-pointer flex font-medium pl-3 pr-3 pt-2 pb-2 gap-2 text-[14px] leading-5 tracking-normal">
+                      <FaGithub className="h-6 w-8" />
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <img
+              src={Proyecto2}
               alt=""
               className="h-[300px] w-auto rounded-2xl max-w-full object-cover"
             />
